be6d8a019f8659d9e7caa00adecf9d9239c8a730
Maciej Szczurek <maciej.szczurek2@gmail.com>
2023-04-28 15:51:07 +0200

    Updated checker for Spring Boot to use the REST API.

--- src/main/java/com/maciejszczurek/updatechecker/checker/SpringBootUpdateChecker.java
+++ src/main/java/com/maciejszczurek/updatechecker/checker/SpringBootUpdateChecker.java
@@ -3,7 +3,10 @@ package com.maciejszczurek.updatechecker.checker;
 import static com.maciejszczurek.updatechecker.application.model.ApplicationType.SPRING_BOOT;
 
 import com.maciejszczurek.updatechecker.checker.annotation.ApplicationType;
+import com.maciejszczurek.updatechecker.util.UpdateCheckerUtils;
 import java.io.IOException;
+import java.net.URI;
+import java.net.URISyntaxException;
 
 @ApplicationType(SPRING_BOOT)
 public class SpringBootUpdateChecker extends UpdateChecker {
@@ -17,11 +20,29 @@ public class SpringBootUpdateChecker extends UpdateChecker {
 
   @Override
   public void checkUpdate() throws IOException {
-    setNewVersion(
-      getJsoupConnectionInstance()
-        .get()
-        .selectFirst("#bootVersion option")
-        .val()
-    );
+    try {
+      final var siteUri = new URI(getSiteUrl());
+
+      setNewVersion(
+        UpdateCheckerUtils
+          .readTree(
+            new URI(
+              siteUri.getScheme(),
+              siteUri.getHost(),
+              "/metadata/client",
+              siteUri.getFragment()
+            )
+              .toURL()
+          )
+          .get("bootVersion")
+          .get("default")
+          .asText()
+      );
+    } catch (InterruptedException e) {
+      Thread.currentThread().interrupt();
+      throw new RuntimeException(e);
+    } catch (URISyntaxException e) {
+      throw new RuntimeException(e);
+    }
   }
 }
