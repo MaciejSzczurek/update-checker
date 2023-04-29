c0a29887f977eaf5f2d56c705a6ebca2abd6d1e4
Maciej Szczurek <maciej.szczurek2@gmail.com>
2023-04-29 08:50:56 +0200

    Added header "Accept" in Spring Boot checker

--- src/main/java/com/maciejszczurek/updatechecker/checker/SpringBootUpdateChecker.java
+++ src/main/java/com/maciejszczurek/updatechecker/checker/SpringBootUpdateChecker.java
@@ -3,10 +3,13 @@ package com.maciejszczurek.updatechecker.checker;
 import static com.maciejszczurek.updatechecker.application.model.ApplicationType.SPRING_BOOT;
 
 import com.maciejszczurek.updatechecker.checker.annotation.ApplicationType;
+import com.maciejszczurek.updatechecker.http.HttpBuilderFactory;
 import com.maciejszczurek.updatechecker.util.UpdateCheckerUtils;
 import java.io.IOException;
 import java.net.URI;
 import java.net.URISyntaxException;
+import java.net.http.HttpRequest;
+import java.net.http.HttpResponse;
 
 @ApplicationType(SPRING_BOOT)
 public class SpringBootUpdateChecker extends UpdateChecker {
@@ -26,13 +29,25 @@ public class SpringBootUpdateChecker extends UpdateChecker {
       setNewVersion(
         UpdateCheckerUtils
           .readTree(
-            new URI(
-              siteUri.getScheme(),
-              siteUri.getHost(),
-              "/metadata/client",
-              siteUri.getFragment()
-            )
-              .toURL()
+            HttpBuilderFactory
+              .getBuilder()
+              .build()
+              .send(
+                HttpRequest
+                  .newBuilder()
+                  .header("Accept", "application/vnd.initializr.v2.2+json")
+                  .uri(
+                    new URI(
+                      siteUri.getScheme(),
+                      siteUri.getHost(),
+                      "/metadata/client",
+                      siteUri.getFragment()
+                    )
+                  )
+                  .build(),
+                HttpResponse.BodyHandlers.ofInputStream()
+              )
+              .body()
           )
           .get("bootVersion")
           .get("default")
