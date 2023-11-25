e7bc8ff9bc878081a97e34cc095d189cb3c8314e
Maciej Szczurek <maciej.szczurek2@gmail.com>
2023-11-25 11:27:17 +0100

    Moved to ThreadPoolTaskExecutorBuilder

--- src/main/java/com/maciejszczurek/updatechecker/task/TaskConfiguration.java
+++ src/main/java/com/maciejszczurek/updatechecker/task/TaskConfiguration.java
@@ -4,7 +4,7 @@ import java.util.Optional;
 import lombok.RequiredArgsConstructor;
 import org.jetbrains.annotations.NotNull;
 import org.springframework.beans.factory.annotation.Value;
-import org.springframework.boot.task.TaskExecutorBuilder;
+import org.springframework.boot.task.ThreadPoolTaskExecutorBuilder;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.scheduling.concurrent.ThreadPoolTaskExecutor;
@@ -18,7 +18,7 @@ public class TaskConfiguration {
 
   @Bean
   public ThreadPoolTaskExecutor applicationTaskExecutor(
-    @NotNull final TaskExecutorBuilder builder
+    @NotNull final ThreadPoolTaskExecutorBuilder builder
   ) {
     return builder
       .corePoolSize(
